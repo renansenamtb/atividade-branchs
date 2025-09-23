afcc9d940b36d47ea223ef975678f19c94b2ca68
renansenamtb <renansenazzz@gmail.com>
2025-09-23 16:24:43 -0400

    trocartexto

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function mudarFundo() {
     document.body.style.backgroundColor =
         document.body.style.backgroundColor === "lightblue" ? "#f0f0f0" : "lightblue";
 }
+function mostrarDataHora() {
+    const agora = new Date();
+    document.getElementById("data-hora").innerHTML = "Agora: " + agora.toLocaleString();
+}
+
