f5483a931d5ae1bc3f13b15e0e5a07bfd1f62da8
renansenamtb <renansenazzz@gmail.com>
2025-09-23 16:18:21 -0400

    cor

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Projeto iniciado!");
+function mudarFundo() {
+    document.body.style.backgroundColor =
+        document.body.style.backgroundColor === "lightblue" ? "#f0f0f0" : "lightblue";
+}
